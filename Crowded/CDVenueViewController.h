f6ca9829dbc955de71433fde99cca9af55700a01
Matt Bridges <matt@intrepid.io>
2014-06-07 14:15:01 -0400

    Initial commit

---
+++ Crowded/CDVenueViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDVenueViewController.h
+//  Crowded
+//
+//  Created by Matt Bridges on 6/7/14.
+//  Copyright (c) 2014 Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDVenueViewController : UIViewController
+
+@end
